65c83d02cdda85e53fa752060de850abc10b4f5d
Andrew Persad <akpersad@gmail.com>
2016-02-16 23:58:48 -0500

    Added background and made responsive

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  root to: 'portfolio#index'
+
+  get 'portfolio/view'
+
+  get 'portfolio/favorite'
+
+  # get 'portfolio/results'
+
+  # post 'portfolio/results'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
